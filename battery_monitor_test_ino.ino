ca80f33870ce286eace0a5d2ce5bbdd7ccbd985a
spencerbug <spencer.neilan@gmail.com>
2014-01-29 08:54:37 -0600

    added event monitoring system
    does not work however.

--- battery_monitor_test_ino.ino
+++ battery_monitor_test_ino.ino
@@ -4,6 +4,8 @@
 
 #include <Wire.h>
 
+#define SHUNT_R 0.02
+
 #define BADDRESS   0xCC //address of all LTC4151 devices
 //register address on device
 #define BSENSEMSB  0x00 //most sig bit sense current 
@@ -14,73 +16,80 @@
 #define BADINLBSB  0x05
 #define BCONTROL   0x06
 
-uint16_t voltage, current;
+void (*i2cHandler)(int);
+
+uint16_t current=5;
+uint16_t voltage=5;
+float realVoltage, realCurrent;
+bool I2CBusy = false;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
-
-}
-
-void I2CHandler( int byteCount)
-Wire.onReceive(I2CInterface::I2CHandler);
-
-
-class I2CInterface{
-  public:
-  bool busy;
-  static void I2CHandler(int byteCount);
-  I2CInterface();
-};
-
-  I2CInterface::I2CInterface(){
-    Wire.onReceive(I2CInterface::I2CHandler); //creates an event handler when receiving an incoming transmission
-    busy=false;
-  }
-  class BatteryMonitorInterface : public I2CInterface{
-    public:
-    uint16_t current;
-    uint16_t voltage;
-    void getAll();
-  }
-  
-  BatteryMonitorInterface
-  
-
-//according to the datasheet, if the master keeps ACKing after each recieved transmission,
-//the slave will continue to send each conscutive with each incrementing address pointer until it hits 0x00
-void getBatteryIV(uint16_t* current, uint16_t* voltage){
-  Wire.requestFrom(BADDRESS, 0x00); //this just requests sens MSB, but master will keep ACKing.
-  Wire.onReceive(batteryMonitorHandler); //sets up an event handler that upon reception, will call BatteryMonitorHandler. 
-          //This overrides quadratureEventHandler.
+  Wire.onReceive((*i2cHandler));
 }
 
 
-void loop() {
-  Wire.requestFrom(BADDRESS,BVOLTSMSB);
-  Wire.onReceive(
-  if(Wire.available() > 7){
-    voltage=Wire.read();    //slave first sends MSB
-    voltage = voltage << 8;  //shift high byte over
-    
-  }
-  while(Wire.available());
 
- =Wire.requestFrom(BADDRESS,BVOLTSLSB);
-  uint16_t voltage_n=(voltageMSB<<8)+voltageLSB;
-  float voltage=102.4/4096.0*(float)voltage_n; //full scale range 0 - 4095 corresponds to 0 to 102.4V
-  
-  uint8_t senseMSB=Wire.requestFrom(BADDRESS,BSENSEMSB);
-  uint8_t senseLSB=Wire.requestFrom(BADDRESS,BSENSELSB);
-  uint16_t sense_n =(voltageMSB<<8)+voltageLSB;
-  float sensevolts=0.08192/4095.0*(float)sense_n;//full scale range 0-4095 corresponds to 0 to 81.92mV
-  float current=sensevolts/0.01;  //i=v/rshunt
+void loop() { 
+//  //uint16_t voltage_n=(voltageMSB<<8)+voltageLSB;
+//  float voltage=102.4/4096.0*(float)voltage_n; //full scale range 0 - 4095 corresponds to 0 to 102.4V
+//  uint16_t sense_n =(voltageMSB<<8)+voltageLSB;
+//  float sensevolts=0.08192/4095.0*(float)sense_n;//full scale range 0-4095 corresponds to 0 to 81.92mV
+//  float current=sensevolts/0.01;  //i=v/rshunt
+//  
+  requestIandV;
   
   Serial.print("voltage: ");
   Serial.print(voltage);
   Serial.print("V.  current: ");
   Serial.print(current);
   Serial.println("A.");
-  
+}
+
+static void getCurrentHandler(int byteCount){
+  current=(Wire.read()<<8) | Wire.read();
+  realCurrent=(0.08192/4095.0*(float)current)/SHUNT_R;//full scale range 0-4095 corresponds to 0 to 81.92mV
+  I2CBusy=false;
+}
+static void getVoltageHandler(int byteCount){
+  voltage=(Wire.read()<<8) | Wire.read();
+  realVoltage=102.4/4096.0*(float)voltage; //full scale range 0 - 4095 corresponds to 0 to 102.4V
+  I2CBusy=false;
+}
+static void getIandVHandler(int byteCout){
+  current=(Wire.read()<<8) | Wire.read();
+  realCurrent=(0.08192/4095.0*(float)current)/SHUNT_R;
+  voltage=(Wire.read()<<8) | Wire.read();
+  realVoltage=102.4/4096.0*(float)voltage; //full scale range 0 - 4095 corresponds to 0 to 102.4V
+  I2CBusy=false;
+    
+}
+
+void requestCurrent(){
+  Wire.beginTransmission(BADDRESS);
+  Wire.write(BSENSEMSB);//write the (starting) register master wants to read from. 
+  Wire.endTransmission();
+  Wire.requestFrom(BADDRESS, 2,false);
+  i2cHandler = &getCurrentHandler;
+  I2CBusy=true;
+}
+
+void requestVoltage(){
+  Wire.beginTransmission(BADDRESS);
+  Wire.write(BVOLTSMSB);
+  Wire.endTransmission();
+  Wire.requestFrom(BADDRESS, 2, false);
+  i2cHandler = &getVoltageHandler;
+  I2CBusy=true;
+}
+
+void requestIandV(){
+  Wire.beginTransmission(BADDRESS);
+  Wire.write(BSENSEMSB);
+  Wire.endTransmission();
+  Wire.requestFrom(BADDRESS, 4, false);
+  i2cHandler = &getIandVHandler;
+  I2CBusy=true;
 }
