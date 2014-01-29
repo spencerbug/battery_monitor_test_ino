7dae196c03246e9208cc5e5ffebb54c686532b25
spencerbug <spencer.neilan@gmail.com>
2014-01-29 14:45:39 -0600

    Revert "No Ack"
    This reverts commit dc94aab9e4064faacd9e0148772175d5103e4218.

--- battery_monitor_test_ino.ino
+++ battery_monitor_test_ino.ino
@@ -1,37 +1,98 @@
+//battery monitor test
+//For interfacing with the LTC4151
+//by spencer neilan, 2014
+
 #include <Wire.h>
 
 #define SHUNT_R 0.02
-#define _LTC4151 0b1100110  //write address
-//#define _LTC4151 205  //read address
-#define _SENSEMSB  0 //most sig bit sense current 
-#define _SENSELSB  1
-#define _VOLTSMSB  2
-#define _VOLTSLSB  3
-#define _ADINMSB   4
-#define _ADINLBSB  5
-#define _CONTROL   6
+#define _LTC4151   0xCC
+#define _SENSEMSB  0x00 //most sig bit sense current 
+#define _SENSELSB  0x01
+#define _VOLTSMSB  0x02
+#define _VOLTSLSB  0x03
+#define _ADINMSB   0x04
+#define _ADINLBSB  0x05
+#define _CONTROL   0x06
 
-uint16_t current, voltage;
+void (*i2cHandler)(int);
+
+uint16_t current=5;
+uint16_t voltage=5;
+float realVoltage, realCurrent;
+bool I2CBusy = false;
 
 void setup() {
+  // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
+  Wire.onReceive((*i2cHandler));
 }
 
-void loop() {
-  Wire.beginTransmission(byte(_LTC4151)); //transmit to write todevic
-  Wire.write(0);  //tell device to point to register 0
-  //delay(15);
-  Wire.requestFrom(_LTC4151, 4);  //request 4 bytes from slave. (0x00,1,2, and 3) in order of sensea, senseb, voltsa, voltsb
-  if(4 <= Wire.available()){
-    Serial.print("available!");
-    current = (Wire.read())<<8 | Wire.read();
-    voltage = (Wire.read())<<8 | Wire.read();
-  }
+
+
+void loop() { 
+//  //uint16_t voltage_n=(voltageMSB<<8)+voltageLSB;
+//  float voltage=102.4/4096.0*(float)voltage_n; //full scale range 0 - 4095 corresponds to 0 to 102.4V
+//  uint16_t sense_n =(voltageMSB<<8)+voltageLSB;
+//  float sensevolts=0.08192/4095.0*(float)sense_n;//full scale range 0-4095 corresponds to 0 to 81.92mV
+//  float current=sensevolts/0.01;  //i=v/rshunt
+//  
+  requestIandV();
+  Serial.print("voltage: ");
   Serial.print(voltage);
-  Serial.print("V ");
+  Serial.print("V.  current: ");
   Serial.print(current);
-  Serial.println("A ");
-  
-  
+  Serial.println("A.");
 }
+
+
+
+static void getIandVHandler(int byteCout){
+  current=(Wire.read()<<8) | Wire.read();
+  realCurrent=(0.08192/4095.0*(float)current)/SHUNT_R;
+  voltage=(Wire.read()<<8) | Wire.read();
+  realVoltage=102.4/4096.0*(float)voltage; //full scale range 0 - 4095 corresponds to 0 to 102.4V
+  I2CBusy=false;
+    
+}
+
+
+void requestIandV(){
+  Wire.beginTransmission(BADDRESS);
+  Wire.write(BSENSEMSB);
+  Wire.endTransmission();
+  Wire.requestFrom(BADDRESS, 4, false);
+  i2cHandler = &getIandVHandler;
+  I2CBusy=true;
+  //Wire.onReceive((getIandVHandler));
+}
+
+//static void getCurrentHandler(int byteCount){
+//  current=(Wire.read()<<8) | Wire.read();
+//  realCurrent=(0.08192/4095.0*(float)current)/SHUNT_R;//full scale range 0-4095 corresponds to 0 to 81.92mV
+//  I2CBusy=false;
+//}
+//static void getVoltageHandler(int byteCount){
+//  voltage=(Wire.read()<<8) | Wire.read();
+//  realVoltage=102.4/4096.0*(float)voltage; //full scale range 0 - 4095 corresponds to 0 to 102.4V
+//  I2CBusy=false;
+//}
+
+//
+//void requestCurrent(){
+//  Wire.beginTransmission(BADDRESS);
+//  Wire.write(BSENSEMSB);//write the (starting) register master wants to read from. 
+//  Wire.endTransmission();
+//  Wire.requestFrom(BADDRESS, 2,false);
+//  i2cHandler = &getCurrentHandler;
+//  I2CBusy=true;
+//}
+//
+//void requestVoltage(){
+//  Wire.beginTransmission(BADDRESS);
+//  Wire.write(BVOLTSMSB);
+//  Wire.endTransmission();
+//  Wire.requestFrom(BADDRESS, 2, false);
+//  i2cHandler = &getVoltageHandler;
+//  I2CBusy=true;
+//}
