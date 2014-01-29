f34f9cdc720efa442bee071727f203ffb2cbce4b
spencerbug <spencer.neilan@gmail.com>
2014-01-29 00:49:14 -0600

    Kind of messed this one up
    Trying to have it so the I2C interface will use an event handler. But I
    want the function it to perform in the event to be variable, dependent
    on which device it's been waiting for. And once it requests data, it
    shouldn't allow other devices that use I2C to use it for the duration.

--- battery_monitor_test_ino.ino
+++ battery_monitor_test_ino.ino
@@ -5,6 +5,7 @@
 #include <Wire.h>
 
 #define BADDRESS   0xCC //address of all LTC4151 devices
+//register address on device
 #define BSENSEMSB  0x00 //most sig bit sense current 
 #define BSENSELSB  0x01
 #define BVOLTSMSB  0x02
@@ -13,23 +14,65 @@
 #define BADINLBSB  0x05
 #define BCONTROL   0x06
 
-
+uint16_t voltage, current;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
+
 }
 
-void loop() {
+void I2CHandler( int byteCount)
+Wire.onReceive(I2CInterface::I2CHandler);
+
+
+class I2CInterface{
+  public:
+  bool busy;
+  static void I2CHandler(int byteCount);
+  I2CInterface();
+};
+
+  I2CInterface::I2CInterface(){
+    Wire.onReceive(I2CInterface::I2CHandler); //creates an event handler when receiving an incoming transmission
+    busy=false;
+  }
+  class BatteryMonitorInterface : public I2CInterface{
+    public:
+    uint16_t current;
+    uint16_t voltage;
+    void getAll();
+  }
   
-  uint16_t voltageMSB=Wire.requestFrom(BADDRESS,BVOLTSMSB);
-  uint16_t voltageLSB=Wire.requestFrom(BADDRESS,BVOLTSLSB);
+  BatteryMonitorInterface
+  
+
+//according to the datasheet, if the master keeps ACKing after each recieved transmission,
+//the slave will continue to send each conscutive with each incrementing address pointer until it hits 0x00
+void getBatteryIV(uint16_t* current, uint16_t* voltage){
+  Wire.requestFrom(BADDRESS, 0x00); //this just requests sens MSB, but master will keep ACKing.
+  Wire.onReceive(batteryMonitorHandler); //sets up an event handler that upon reception, will call BatteryMonitorHandler. 
+          //This overrides quadratureEventHandler.
+}
+
+
+void loop() {
+  Wire.requestFrom(BADDRESS,BVOLTSMSB);
+  Wire.onReceive(
+  if(Wire.available() > 7){
+    voltage=Wire.read();    //slave first sends MSB
+    voltage = voltage << 8;  //shift high byte over
+    
+  }
+  while(Wire.available());
+
+ =Wire.requestFrom(BADDRESS,BVOLTSLSB);
   uint16_t voltage_n=(voltageMSB<<8)+voltageLSB;
   float voltage=102.4/4096.0*(float)voltage_n; //full scale range 0 - 4095 corresponds to 0 to 102.4V
   
-  uint16_t senseMSB=Wire.requestFrom(BADDRESS,BSENSEMSB);
-  uint16_t senseLSB=Wire.requestFrom(BADDRESS,BSENSELSB);
+  uint8_t senseMSB=Wire.requestFrom(BADDRESS,BSENSEMSB);
+  uint8_t senseLSB=Wire.requestFrom(BADDRESS,BSENSELSB);
   uint16_t sense_n =(voltageMSB<<8)+voltageLSB;
   float sensevolts=0.08192/4095.0*(float)sense_n;//full scale range 0-4095 corresponds to 0 to 81.92mV
   float current=sensevolts/0.01;  //i=v/rshunt
