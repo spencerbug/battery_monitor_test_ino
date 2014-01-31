ad76ee8ef0158677db117db48eead1a50e8c50fe
spencerbug <spencer.neilan@gmail.com>
2014-01-31 08:56:42 -0600

    Quadrature Test Still not Functional
    Slave pulls SDA line to ground. Need to order new LT4151's fast.

--- battery_monitor_test_ino.ino
+++ battery_monitor_test_ino.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 
-#define SHUNT_R 0.02
-#define _LTC4151 0b1100110  //write address
+#define _LTC4151   0x66  //write address
 //#define _LTC4151 205  //read address
 #define _SENSEMSB  0 //most sig bit sense current 
 #define _SENSELSB  1
@@ -21,13 +20,15 @@ void setup() {
 void loop() {
   Wire.beginTransmission(byte(_LTC4151)); //transmit to write todevic
   Wire.write(0);  //tell device to point to register 0
-  delay(15);
-  Wire.requestFrom(_LTC4151, 4);  //request 4 bytes from slave. (0x00,1,2, and 3) in order of sensea, senseb, voltsa, voltsb
-  if(4 <= Wire.available()){
-    Serial.print("available!");
+  //delay(15);
+  Wire.requestFrom(_LTC4151, 4, false);  //request 4 bytes from slave. (0x00,1,2, and 3) in order of sensea, senseb, voltsa, voltsb
+  //false is for repeated start, as specified by the datasheet.
+  if(4 <= Wire.available()){  //if 4 bytes were received
+    //Serial.print(Wire.available());
     current = (Wire.read())<<8 | Wire.read();
     voltage = (Wire.read())<<8 | Wire.read();
   }
+  Wire.endTransmission();
   Serial.print(voltage);
   Serial.print("V ");
   Serial.print(current);
